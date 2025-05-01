5b8ff031b81c6b3bdb2de88b58c47b06e6529c0e
jaydeshmukh078 <jaydeshmukh0010@gmail.com>
2025-05-01 19:30:49 +0530

    first commit

---
+++ day2.js
@@ -0,0 +1,17 @@
+'use strict'   // 2015 es6 update
+
+var num1 = 6
+var num2 = 5
+
+console.log(num1+num2)
+console.log(num1-num2)
+console.log(num1*num2)
+console.log(num1/num2)
+console.log(num1%num2)
+console.log(num1**num2)
+
+
+var a=2.0
+var b=8
+
+console.log(a+b)
