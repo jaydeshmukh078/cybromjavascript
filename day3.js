e76ff1e0907fdfd47db88de8602196f805366abd
jaydeshmukh078 <jaydeshmukh0010@gmail.com>
2025-05-02 19:36:49 +0530

    addition

---
+++ day3.js
@@ -0,0 +1,36 @@
+"use strict"
+
+// function shubh(){
+//     console.log("welcome to my first function")
+//     var a=10
+//     var b=20
+//     console.log( "sum of a + b = " + (a+b) )
+// }
+// function jay(){
+//     console.log("welcome to my Second function")
+// }
+
+// jay()
+// shubh()
+
+
+// -------------------------------------------------------------------------------------------------------------------------------
+
+// alert("THis is alert method")
+
+// var ans = window.confirm("are you okay")
+// console.log(ans);
+
+// var ans = window.prompt("are you 18+ ")
+// console.log(ans);
+
+// prompt("are you 18+ " + "")
+
+//  HOME WORK
+
+// 2 prompt addition program and give ans in promt
+
+var a = prompt("enter first number")
+var b = prompt("enter second number")
+
+prompt()
