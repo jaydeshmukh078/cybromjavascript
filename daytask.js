317e78d378b128dae994c6cbaade95b9073a599a
jaydeshmukh078 <jaydeshmukh0010@gmail.com>
2025-05-10 19:40:28 +0530

    dayyyyyy

---
+++ daytask.js
@@ -0,0 +1,15 @@
+function first() {
+    alert("first function called")
+}
+function second() {
+    alert("second function called")
+}
+function third() {
+    alert("third function called")
+}
+
+// substring
+// tolowercase
+// touppercase
+// Charat 
+// charcode
