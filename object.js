66e7c369302e61b5b0db88f87ea1342f544338cd
jaydeshmukh078 <jaydeshmukh0010@gmail.com>
2025-05-19 19:32:58 +0530

    loss

---
+++ object.js
@@ -0,0 +1,72 @@
+// // -------------------normal function-------------------------------------------
+// function okk() {
+//     console.log("normal function");
+
+// }
+// okk();
+
+
+
+
+// // -------------------anonymus function-------------------------------------------
+// let ano = function () {
+//     console.log("heyy i am anoymus function");
+
+// }
+// ano();
+
+
+
+// // -------------------IIFE function-------------------------------------------
+// // -------------------immediet invoke function expression-------------------------------------------
+// (function () {
+//     console.log("IIFE function");
+// })()
+
+
+
+// // -------------------Arrow function(special type of anonymus function)-------------------------------------------
+// let arrow = () =>
+// {
+//     console.log("arrow function");
+// }
+// arrow();
+
+
+
+// let anshul=()=>{
+//     alert("hey i am first alert")
+// }
+
+
+// let jay=()=>{
+//     alert("hey i am second alert")
+// }
+
+
+
+// -------------------Object------------------------------------------- 
+let obj = {
+    name: "anshul",
+    // name: "jay",   // always uniqe
+    age: 25,
+    city: "delhi",
+    state: "uttar pradesh",
+    hobbies: ["cricket", "football", "reading"],
+    fun:function() {
+        console.log("welcome to the function");
+    }
+}
+obj.fun()
+console.log(obj.name);
+obj.college = "RGPV"     // add data
+console.log(obj.city);
+console.log(obj.hobbies[0]);   // array access
+delete obj.name  // delete data
+obj.age = 26   //update data
+console.log(obj);
+
+
+
+
+// ----------------------------------------------
