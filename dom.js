02c350a0aba7bef85dcd721a831db0fc3ee49ca2
jaydeshmukh078 <jaydeshmukh0010@gmail.com>
2025-05-22 19:30:40 +0530

    updatee

---
+++ dom.js
@@ -0,0 +1,24 @@
+'use strict'
+
+let myname=()=>{
+
+    document.querySelector("#myname").innerHTML = "Jay";
+    document.querySelector("#myname1").innerHTML = "Jay deshmukh";
+    
+}
+
+let surname=()=>{
+
+   let mytag = document.querySelector(".surname")
+   let mytag1 = document.querySelector(".surname")
+
+  mytag.innerHTML = "jay Deshmukh";
+  mytag1.innerHTML = "jay";
+
+
+  // make 3 button already write a string 
+  // change color (red button to red)
+  // change color (green button to green)
+  // change color (blue button to blue)
+    
+}
